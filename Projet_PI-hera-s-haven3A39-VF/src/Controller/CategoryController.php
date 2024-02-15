1cf7c98f53f93e47564bd534e892d938a48798b9
yasmeene11 <yasminekarray526@gmail.com>
2024-02-15 08:53:59 +0100

    productCRUD+controlesaisie

---
+++ Projet_PI-hera-s-haven3A39-VF/src/Controller/CategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\FormCategoryType;
+use App\Repository\CategoryRepository;
+use App\Form\CategoryType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/List_cat', name: 'app_listcat')]
+    public function Listcat(CategoryRepository $repo): Response
+    {
+        $result = $repo->findAll();
+        return $this->render('/Back/category/Listcat.html.twig', [
+            'result' => $result,
+        ]);
+    }
+    #[Route('/add_cat', name: 'app_add_cat')]
+    public function Addcat(ManagerRegistry $mr,Request $req): Response
+    {
+        $s=new Category();
+        $form=$this->createForm(FormCategoryType::class,$s);
+        $form->handleRequest($req);
+        if($form->isSubmitted()&& $form->isValid()){
+         $em=$mr->getManager();
+         $em->persist($s);
+         $em->flush();
+         return $this->redirectToRoute('app_listcat');
+        }
+        return $this->render('/Back/Category/Addcat.html.twig', [
+            'formCategory'=>$form->createView()
+        ]);
+    }
+    #[Route('/update/{categoryId}', name: 'updatecat')]
+    public function updatecat($categoryId,CategoryRepository $repo, ManagerRegistry $mr, Request $req):Response
+    {
+    $s=$repo->find($categoryId) ;
+    $form=$this->createForm(FormCategoryType::class,$s) ; 
+        $form->handleRequest($req);
+    if($form->isSubmitted() && $form->isValid())  {
+        $em=$mr->getManager();
+        $em->persist($s);
+        $em->flush();
+       return $this->redirectToRoute('app_listcat');
+    }
+    return $this->renderForm('/Back/Category/Updatecat.html.twig',[
+        'formCategory'=>$form
+    ]);
+}
+    #[Route('/remove/{categoryId}', name: 'removecat')]
+    public function removecat($categoryId,ManagerRegistry $mr, CategoryRepository $repo) :Response
+    {
+       $category=$repo->find($categoryId); 
+        $em=$mr->getManager();
+        $em->remove($category);
+        $em->flush();
+        return $this->redirectToRoute('app_listcat');
+    }
+}
---
+++ Projet_PI-hera-s-haven3A39-VF/src/Form/FormProductType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+//use App\Entity\Category;
+use Symfony\Component\Form\Extension\Core\Type\TextType ;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Product_Name', TextType::class, [
+                'label' => 'Product_Name',
+                'attr' => [
+                    'placeholder' => 'Enter product name',
+                ],
+                'required'=> false,
+            ])
+            ->add('Product_Quantity', TextType::class, [
+                'label' => 'Product_Quantity',
+                'attr' => [
+                    'placeholder' => 'Enter product quantity',
+                ],
+                'required'=> false,
+
+            ])
+            ->add('Product_Label', TextType::class, [
+                'label' => 'Product_Label',
+                'attr' => [
+                    'placeholder' => 'Enter product label',
+                ],
+                'required'=> false,
+
+            ])
+            ->add('Expiration_Date')
+            ->add('Category_Key')
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
