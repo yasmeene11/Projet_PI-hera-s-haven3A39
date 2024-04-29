309d11bdb23e8e39c29a00442da46765ba1b38be
Adem-Belguith <adem.belguith@esprit.tn>
2024-04-29 14:08:22 +0100

    added a part of reset pass

--- src/main/java/test/MainFX.java
+++ src/main/java/test/MainFX.java
@@ -13,7 +13,7 @@ import java.awt.*;
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fronnt/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fronntianana/login.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
