be219323f7d857e41657a42fb41f74804dc71981
Adem-Belguith <adem.belguith@esprit.tn>
2024-04-29 14:07:46 +0100

    added sign in with google

--- src/main/java/test/MainFX.java
+++ src/main/java/test/MainFX.java
@@ -13,7 +13,7 @@ import java.awt.*;
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fronnttttggggg/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fronnt/login.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
